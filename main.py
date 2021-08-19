30f654fd91f075c139dc5aba288708b9d436cf92
Taiblockzero <taiblockzero@gmail.com>
2021-08-19 12:56:14 +0300

    add mode 2 to read the list in english with tts, mode 3 same in japanese

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import gtts
 import requests
 import random
+from gtts import gTTS
+import os
 
 randomAnimeNames = []
 while len(randomAnimeNames) < 10:
@@ -14,7 +17,7 @@ while len(randomAnimeNames) < 10:
             except KeyError:
                 pass
 
-mode = input("Write 1 for write-to-file mode, any other key for print only:")
+mode = input("Write 1 for write-to-file mode, 2 for english tts mode, 3 for japanese tts mode, any other key for print only:")
 
 if mode == '1':
     f = open("C:\\Users\\Ivo\\Documents\\Python\\RandomAnimeListPy\\RandomAnimeList.txt", "a")
@@ -22,5 +25,15 @@ if mode == '1':
         f.write(i)
         f.write("\n")
     f.close()
+
+if mode == '2':
+    listStr = ','.join(randomAnimeNames)
+    myobj = gTTS(text=listStr, lang="en", slow=False)
+    myobj.save("C:\\Users\\Ivo\\Documents\\Python\\RandomAnimeListPy\\tts.mp3")
+
+if mode == '3':
+    listStr = ','.join(randomAnimeNames)
+    myobj = gTTS(text=listStr, lang="ja", slow=False)
+    myobj.save("C:\\Users\\Ivo\\Documents\\Python\\RandomAnimeListPy\\tts.mp3")
 
 print(randomAnimeNames)
